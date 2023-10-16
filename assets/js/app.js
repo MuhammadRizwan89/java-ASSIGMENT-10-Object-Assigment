e99021b4ac5a779110b2163bcc253c558667300b
MuhammadRizwan89 <mrizwansaeed89xclusive@gmail.com>
2023-10-16 17:41:36 +0500

    javaScript object assigment post 16 oct 2023

---
+++ assets/js/app.js
@@ -0,0 +1,390 @@
+let cars = {
+  Honda: {
+    civic: {
+      reborn: {
+        src: "./assets/images/honda civic reborn.webp",
+        name: "Honda Civic Reborn",
+        year: "Year 2023",
+        Price: "1M 200k",
+        Colors: ["silver shine", "black", "white"],
+        gears: "5 gear system Auto and Manual",
+      },
+    },
+  },
+  Toyyota: {
+    corolla: {
+      Gli: {
+        src: "./assets/images/toyyota crola gli.jpg",
+        name: "Toyyota Corolla Gli",
+        year: "Year 2023",
+        Price: "6M 170k",
+        Colors: ["red", "black", "white"],
+        gears: "5 gear system Manual",
+      },
+    },
+  },
+  Suzuki: {
+    swift: {
+      maruti: {
+        src: "./assets/images/suzuki swift maruti.jfif",
+        name: "Suzuki Swift Maruti",
+        year: "Year 2023",
+        Price: "4M 256k",
+        Colors: ["red", "blue", "white"],
+        gears: "GLX CVT variant",
+      },
+    },
+    Wagon_R: {
+      Detail: {
+        src: "./assets/images/Suzuki Wagon R.webp",
+        name: "Suzuki Wagon R",
+        year: "Year 2023",
+        Price: "3M 200k",
+        Colors: ["red", "blue", "white"],
+        gears: "Auto Gear Shift",
+      },
+    },
+    Vitara: {
+      Detail: {
+        src: "./assets/images/Suzuki Vitara.jpeg",
+        name: "Suzuki Vitara",
+        year: "Year 2023",
+        Price: "3M 200k",
+        Colors: ["red", "blue", "white"],
+        gears: "Auto Gear Shift",
+      },
+    },
+    Baleno: {
+      Detail: {
+        src: "./assets/images/suzuki-baleno.jpg",
+        name: "Suzuki Baleno",
+        year: "Year 2023",
+        Price: "770k",
+        Colors: ["red", "blue", "orange", "gray"],
+        gears: " 4-speed automatic transmission",
+      },
+    },
+    Lgnis: {
+      Detail: {
+        src: "./assets/images/Suzuki Lgnis.webp",
+        name: "Suzuki Lgnis",
+        year: "Year 2023",
+        Price: "2M 700k",
+        Colors: ["yellow", "Orange", "green"],
+        gears: "five-speed manual or a continuously-variable transmission",
+      },
+    },
+    Ertiga: {
+      Detail: {
+        src: "./assets/images/Suzuki Ertiga.webp",
+        name: "Suzuki Ertiga",
+        year: "Year 2023",
+        Price: "7M 500k",
+        Colors: ["Brown", "red", "white"],
+        gears: "4-speed automatic transmission",
+      },
+    },
+  },
+  Nissan: {
+    Datsun: {
+      Go_plus: {
+        src: "./assets/images/Nissan Datsun Go+.avif",
+        name: "Nissan Datsun Go+",
+        year: "Year 2023",
+        Price: "1M 76k",
+        Colors: ["red", "black", "brown"],
+        gears: "5 gear system Manual",
+      },
+    },
+  },
+  Suv: {
+    Land_Rover: {
+      Discovery_Sport: {
+        src: "./assets/images/Land Rover Discovery Sport.jpeg",
+        name: "Land Rover Discovery Sport",
+        year: "Year 2023",
+        Price: "5M 437k",
+        Colors: ["orange", "red", "off-white"],
+        gears: "9–speed ZF 9-HP automatic",
+      },
+    },
+  },
+  Crossover: {
+    Volvo_S60: {
+      Cross_Country: {
+        src: "./assets/images/Crossover Volvo s60 Cross Country.webp",
+        name: "Crossover Volvo s60 Cross Country",
+        year: "Year 2023",
+        Price: "8M 286k",
+        Colors: ["silver", "black", "brown"],
+        gears: "6 gear system Manual",
+      },
+    },
+  },
+  Coupe: {
+    Mercedes_Benz: {
+      GLE_Coupe: {
+        src: "./assets/images/Coupe Mercedez Benz Gle Coupe.webp",
+        name: "Coupe Mercedez Benz Gle Coupe",
+        year: "Year 2023",
+        Price: "2Billion 3Million",
+        Colors: ["red", "blue", "white"],
+        gears: "9G-Tronic automatic gearbox ",
+      },
+    },
+  },
+  Convertible: {
+    Audi: {
+      A3_Cabriolet: {
+        src: "./assets/images/Convertible Audi A3 Cabriolet.webp",
+        name: "Convertible Audi A3 Cabriolet",
+        year: "Year 2023",
+        Price: "11 Billion",
+        Colors: ["red", "blue", "yellow"],
+        gears: "Audi A3 Cabriolet transmission",
+      },
+    },
+  },
+  porsche: {
+    911: {
+      dakar: {
+        src: "./assets/images/Porsche 911 dakar.webp",
+        name: "Porsche 911 dakar",
+        year: "Year 2023",
+        Price: "12Billion 900k",
+        Colors: ["Green", "blue", "white"],
+        gears: "8-speed Porsche Doppelkupplung",
+      },
+    },
+  },
+  Lamborghini: {
+    Huracan: {
+      Sterrato: {
+        src: "./assets/images/Lamborghini Huracan Sterrato.webp",
+        name: "Lamborghini Huracan Sterrato",
+        year: "Year 2023",
+        Price: "7Billion 9M 600.5k",
+        Colors: ["orange", "Yellow", "Green"],
+        gears: "gearboxDual clutch gearbox LDF",
+      },
+    },
+  },
+  Range_Rover: {
+    sport: {
+      sv: {
+        src: "./assets/images/Range Rover sport sv.avif",
+        name: "Range Rover sport sv",
+        year: "Year 2023",
+        Price: "11Billion 800k",
+        Colors: ["silver shine", "black", "white"],
+        gears: " eight-speed automatic transmission",
+      },
+    },
+  },
+  Mercedes: {
+    AMG: {
+      C63: {
+        src: "./assets/images/Mercedes AMG c63.jpeg",
+        name: "Mercedes AMG c63",
+        year: "Year 2023",
+        Price: "1Billion 700.5k",
+        Colors: ["Red", "Gray", "Blue"],
+        gears: "9-speed automatic transmission",
+      },
+    },
+  },
+  BMW: {
+    Touring: {
+      M3: {
+        src: "./assets/images/BMW M3 Touring.jpeg",
+        name: "BMW M3 Touring",
+        year: "Year 2023",
+        Price: "2Billion 7Million 184k",
+        Colors: ["Blue", "Green", "white", "Red"],
+        gears: "sharp-shifting eight-speed automatic transmission",
+      },
+    },
+  },
+  Hyundai: {
+    Ioniq: {
+      five_N: {
+        src: "./assets/images/Hyundai Ioniq 5N.jpg",
+        name: "Hyundai Ioniq 5N",
+        year: "Year 2023",
+        Price: "9 Billion 4 Million 600k",
+        Colors: ["Sky Blue", "Gray", "white"],
+        gears: "Simulated Eight-Speed Gearbox",
+      },
+    },
+  },
+  Lotus: {
+    Emira: {
+      AMG: {
+        src: "./assets/images/Lotus Emira AMG.avif",
+        name: "Lotus Emira AMG",
+        year: "Year 2023",
+        Price: "1Billion 6Million 400k",
+        Colors: ["Yellow", "brown", "Blue"],
+        gears: "AMG's eight-speed dual clutch gearbox",
+      },
+    },
+  },
+  Alfa_Romeo: {
+    supercar: {
+      six_c: {
+        src: "./assets/images/Alfa Romeo Supercar Six c.webp",
+        name: "Alfa Romeo Supercar Six c",
+        year: "Year 2023",
+        Price: "launch date october 2023 price not releas",
+        Colors: ["Red", "Green", "white"],
+        gears: " six speed Alfa TCT Dual Dry Clutch Transmission",
+      },
+    },
+  },
+  Fiat: {
+    Panda: {
+      EV: {
+        src: "./assets/images/Fiat Panda EV.jpeg",
+        name: "Fiat Panda EV",
+        year: "Year 2023",
+        Price: "8Million 100k",
+        Colors: ["silver shine", "black", "white"],
+        gears: "Gear Box, Single Speed. Steering",
+      },
+    },
+  },
+  Pagani: {
+    Utopia: {
+      Utopia: {
+        src: "./assets/images/Pagani Utopia.webp",
+        name: "Pagani Utopia",
+        year: "Year 2023",
+        Price: "1Trillion 60Billion 7Million 700.5k",
+        Colors: ["silver shine", "black", "white"],
+        gears: "7-speed transversal gearbox outmatic and manual",
+      },
+    },
+  },
+  Maserati: {
+    Folgore: {
+      MC20: {
+        src: "./assets/images/Maserati Folgore Mc20.webp",
+        name: "Maserati Folgore Mc20",
+        year: "Year 2023",
+        Price: "13 billion",
+        Colors: ["Perple", "Gray", "Sky Blue"],
+        gears: "The electric Maserati GranTurismo ",
+      },
+    },
+  },
+  Gordon: {
+    Murray: {
+      Spider: {
+        src: "./assets/images/Gordon Murray Spider.jpg",
+        name: "Gordon Murray Spider",
+        year: "Year 2023",
+        Price: "$2.35 million",
+        Colors: ["Blue", "black", "white"],
+        gears: "six-speed gearbox",
+      },
+    },
+  },
+  McLaren: {
+    Artura: {
+      Spider: {
+        src: "./assets/images/McLaren Artura Spider.webp",
+        name: " McLaren Artura Spider ",
+        year: "Year 2023",
+        Price: "300 Million",
+        Colors: ["Golden", "Orange", "white"],
+        gears: "5 gear system Auto and Manual",
+      },
+    },
+  },
+};
+
+let company = document.getElementById("company");
+let brand = document.getElementById("brand");
+let allCars = document.getElementById("allCars");
+
+company.innerHTML = `<option value="all">All</option>`;
+brand.innerHTML = `<option value="">Select model</option>`;
+allCars.innerHTML = "";
+
+for (var key in cars) {
+  company.innerHTML += `<option value="${key}">${key.toUpperCase()}</option>`;
+}
+
+function companyChange() {
+  brand.innerHTML = `<option value="">Select model</option>`;
+  allCars.innerHTML = "";
+
+  var selectedCompany = company.value;
+  if (selectedCompany === "all") {
+    for (var key in cars) {
+      for (var key1 in cars[key]) {
+        for (var key2 in cars[key][key1]) {
+          displayCarData(cars[key][key1][key2]);
+        }
+      }
+    }
+  } else {
+    for (var key in cars[selectedCompany]) {
+      brand.innerHTML += `<option value="${key}">${key.toUpperCase()}</option>`;
+    }
+  }
+}
+
+function filterCars() {
+  var selectedCompany = company.value;
+  var selectedModel = brand.value;
+  allCars.innerHTML = "";
+
+  if (selectedCompany === "all") {
+    for (var key in cars) {
+      for (var key1 in cars[key]) {
+        for (var key2 in cars[key][key1]) {
+          displayCarData(cars[key][key1][key2]);
+        }
+      }
+    }
+  } else {
+    var carData = cars[selectedCompany][selectedModel];
+    if (carData) {
+      for (var i in carData) {
+        displayCarData(carData[i]);
+      }
+    } else {
+      allCars.innerText = "please select car model";
+    }
+  }
+}
+
+function displayCarData(allCar) {
+  allCars.innerHTML += `
+  <div class="col-md-4 mb-4">
+  <div class="card animate__animated animate__zoomIn" style="animation-duration: 1s;">
+    <div class="card-body">
+      <div class="img_holder"><img class="card-img-top" src="${
+        allCar.src
+      }" alt="Car image"></div>
+      <h1 class="card-title">${allCar.name} <span class="title_year">${
+    allCar.year
+  }</span></h1>
+      <h6>${allCar.gears}</h6>
+      <h3>${allCar.Price}</h3>
+      <div class="color-holder">
+        ${displayColors(allCar.Colors)}
+      </div>
+    </div>
+  </div>
+</div>`;
+
+  function displayColors(colors) {
+    let colorSpans = "";
+    colors.forEach((coloritem) => {
+      colorSpans += `<span class="color_sp" style="background-color: ${coloritem};"></span>`;
+    });
+    return colorSpans;
+  }
+}
